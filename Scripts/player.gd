f3718f822e7d66b440ba6da687accaa44df22308
vexruna <vexruna@gmail.com>
2026-04-04 16:15:01 +0300

    Init

---
+++ Scripts/player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+@export var SPEED := 500.0
+@export var ACCELERATION := 1920.0
+@export var FRICTION := 1920.0
+@export var JUMP_VELOCITY := -700.0
+@export var DASH_SPEED := 1200.0
+var DashToken = 1
+var DashLimit = 1
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor() and $DashTimer.is_stopped():
+		velocity += get_gravity() * delta
+	# Handle jump.
+	if Input.is_action_just_pressed("action_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Vector2()
+	direction = Input.get_vector("action_left", "action_right","action_up", "action_down")
+	var velocity_weight: float = (ACCELERATION if direction else FRICTION) * delta
+	
+	if direction.x:
+		$Placeholder/Face.set_skew(deg_to_rad(13.5))
+		$Placeholder.scale.x = round(direction.x)
+	else:
+		$Placeholder/Face.set_skew(deg_to_rad(0))
+
+	if direction.x and $DashTimer.is_stopped():
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, velocity_weight)
+	else:
+		if $DashTimer.is_stopped():
+			velocity.x = move_toward(velocity.x, 0, velocity_weight)
+
+	#Dash Indicator
+	$DashText.text = str($DashTimer.time_left).erase(3, 20)
+	if not $DashTimer.is_stopped():
+		$Placeholder.set_modulate(Color(0.129, 0.933, 0.0))
+	if $DashTimer.is_stopped() and not is_on_floor() and DashToken <= 0:
+		$Placeholder.set_modulate(Color("Red"))
+	if $DashTimer.is_stopped() and is_on_floor():
+		$Placeholder.set_modulate(Color("White"))
+	#Direction Indicator
+	$VectorText.text = "Direction: " + str(direction)
+
+# Dash mechanic WIP
+	if is_on_floor():
+		DashToken = DashLimit
+	if Input.is_action_just_pressed("action_dash") and $DashTimer.is_stopped() and DashToken > 0:
+		velocity = Vector2(0, 0)
+		$DashTimer.start()
+		DashToken -= 1
+		if direction:
+			velocity = direction.normalized() * DASH_SPEED
+		else:
+			velocity.x += DASH_SPEED * $Placeholder.scale.x
+
+	move_and_slide()
