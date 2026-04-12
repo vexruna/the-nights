d8d50a6559671e9c0b47b5a0a607a226d8960c68
vexruna <vexruna@gmail.com>
2026-04-12 14:23:10 +0300

    State Machine Achieved.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,99 +3,166 @@ extends CharacterBody2D
 @export var SPEED := 600.0
 @export var ACCELERATION := 5000.0
 @export var FRICTION := 5000.0
-@export var JUMP_VELOCITY := -800.0
+@export var FALL_VELOCITY := -800.0
 @export var TERMINAL_VELOCITY := 3000.0
 @export var DASH_SPEED := Vector2(1200, 800)
 
 var direction := Vector2()
-var DashToken = 1
-var DashLimit = 1
+var dash_token = 1
+var dash_limit = 1
 var topvelocity := Vector2(0.0, 0.0)
-var koyote:= true
-var goat:= false
-var wall:= Vector2(0, 0)
+enum States
+{
+	NEUTRAL,
+	FALL,
+	DASH,
+	ATTACK,
+	DEFLECT
+}
+enum Surface
+{
+	GROUND,
+	WALL
+}
+var _state : States = States.NEUTRAL
+var _last_touched_surface: Surface
 
+func _ready():
+	pass
 func _physics_process(delta):
-	PlayerMovement(delta)
-	Indicator()
-	Debug()
+	state_machine(delta)
+	indicator()
+	debug()
 	move_and_slide()
+func state_machine(delta):
+	match _state:
+		States.NEUTRAL:
+			state_neutral(delta)
+		States.FALL:
+			state_fall(delta)
+		States.DASH:
+			state_dash(direction)
+		States.ATTACK:
+			state_attack()
+		States.DEFLECT:
+			state_deflect()
 
-func PlayerMovement(delta):
+func set_state(new_state: States): # DRY.
+	if new_state == _state:
+		return
+	_state = new_state
+
+func state_neutral(delta): # Stand, walk and enjoy laws of the universe.
+	walk(delta)
+	gravity(delta)
+	koyote()
+	
+	if is_on_floor():
+		_last_touched_surface = Surface.GROUND
+		$KoyoteTimer.start()
+		dash_token = dash_limit # This resets you ability to dash.
+	if is_on_wall_only():
+		_last_touched_surface = Surface.WALL
+		$WallJumpTimer.start()
+	
+	if Input.is_action_just_pressed("action_jump"):
+		jump(_last_touched_surface)
+	if Input.is_action_just_pressed("action_dash"):
+		dash()
+
+func state_fall(delta): # You're now free falling.
+	walk(delta)
+	gravity(delta)
+	if is_on_floor() or is_on_wall_only():
+		set_state(States.NEUTRAL)
+	if Input.is_action_just_pressed("action_dash"):
+		dash()
+
+func state_dash(to: Vector2): # Defy gravity, move fast and avoid damage.
+	if to:
+		velocity = to.normalized() * DASH_SPEED
+	else:
+		velocity.x = DASH_SPEED.x * $Placeholder.scale.x
+	if $DashTimer.is_stopped():
+		set_state(States.NEUTRAL)
+
+func state_attack(): # TODO: Deal damage, finish posture broken opponents.
+	pass
+
+func state_deflect(): # TODO: Prevent taking damage and deal posture damage in return also send back projectiles.
+	pass
+
+func walk(delta):
 	# Get the input direction and handle the movement/deceleration.
 	direction = Input.get_vector("action_left", "action_right","action_up", "action_down")
 	var velocity_weight: float = (ACCELERATION if direction else FRICTION) * delta
 	
-	# Koyote + Air Drag
-	if is_on_floor() and not koyote:
-		$KoyoteTimer.stop()
-		koyote = true
-		ACCELERATION = 5000.0
-		FRICTION = 5000.0
-	if not is_on_floor() and koyote:
-		koyote = false
-		$KoyoteTimer.start()
-	
-	# Gravity.
-	if not is_on_floor() and $DashTimer.is_stopped() and $KoyoteTimer.is_stopped():
-		# Wall friction
-		ACCELERATION = 2000.0
-		FRICTION = 2000.0
-		if is_on_wall_only() and velocity.y > 400 and not direction.y == 1:
-			velocity.y = 400
-		else:
-			velocity += get_gravity() * delta
-			if velocity.y > TERMINAL_VELOCITY:
-				velocity.y = TERMINAL_VELOCITY
-			
-
-	# Handle jump.
-	if Input.is_action_just_pressed("action_jump"):
-		if is_on_floor() or not is_on_floor() and $KoyoteTimer.time_left > 0:
-			velocity.y = JUMP_VELOCITY
-			koyote = false
-			$KoyoteTimer.stop()
-		if is_on_wall_only() or not is_on_wall_only() and $KoyoteTimer.time_left > 0:
-			wall = get_wall_normal()
-			velocity.x = JUMP_VELOCITY * 0.8 * -wall.x
-			velocity.y = JUMP_VELOCITY
-			koyote = false
-			$KoyoteTimer.stop()
-
-
-
-	#Basic movement
-	if direction.x and $DashTimer.is_stopped():
+		# Smooth accel/decel movement.
+	if direction.x:
 		velocity.x = move_toward(velocity.x, roundf(direction.x) * SPEED, velocity_weight)
 	else:
-		if $DashTimer.is_stopped():
-			velocity.x = move_toward(velocity.x, 0, velocity_weight)
+		velocity.x = move_toward(velocity.x, 0, velocity_weight)
 
-# Dash mechanic WIP
-	if is_on_floor() and DashToken == 0:
-		DashToken = DashLimit
-	if Input.is_action_just_pressed("action_dash") and $DashTimer.is_stopped() and $DashDelay.is_stopped() and DashToken > 0:
-		velocity = Vector2.ZERO
+func jump(from: Surface): # They see me jumpin', they hatin'
+	if $KoyoteTimer.time_left > 0 or $WallJumpTimer.time_left > 0:
+		match from:
+			Surface.GROUND:
+				velocity.y = FALL_VELOCITY
+			Surface.WALL:
+				var wall := get_wall_normal()
+				velocity.y = FALL_VELOCITY
+				velocity.x = FALL_VELOCITY * -wall.x
+		$KoyoteTimer.stop()
+		$WallJumpTimer.stop()
+		set_state(States.FALL)
+
+func dash(): # Gravity need not apply.
+	if $DashDelay.is_stopped() and dash_token > 0:
+		dash_token -= 1
 		$DashTimer.start()
 		$DashDelay.start()
-		$KoyoteTimer.stop()
-		DashToken -= 1
-		if direction:
-			velocity = direction.normalized() * DASH_SPEED
-		else:
-			velocity.x = DASH_SPEED.x * $Placeholder.scale.x
+		set_state(States.DASH)
 
-func Indicator(): # For testing and debugging
-	# Dash Christmas tree
+func gravity(delta): # Applies global world gravity to the player.
+	if is_on_floor():
+		air_drag(false)
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		air_drag(true)
+		if is_on_wall_only() and velocity.y > 400 and direction.y < 1:
+			velocity.y = 400
+		if velocity.y > TERMINAL_VELOCITY:
+			velocity.y = TERMINAL_VELOCITY
+
+func koyote(): # I can fly now.
+	if is_on_floor():
+		return
+	if $KoyoteTimer.time_left > 0:
+		if velocity.y > 0:
+			velocity.y = 0
+
+func air_drag(on: bool): # This will prevent climbing up walls.
+	if on:
+		ACCELERATION = 2000.0
+		FRICTION = 2000.0
+	else:
+		ACCELERATION = 5000.0
+		FRICTION = 5000.0
+
+func indicator(): # For testing and differentiating all states the player could be in.
 	if not $DashTimer.is_stopped():
-		$Placeholder.set_modulate(Color("Green"))
-	if $DashTimer.is_stopped() and not $DashDelay.is_stopped() or $KoyoteTimer.time_left > 0:
+			$Placeholder.set_modulate(Color("Green"))
+	if $DashTimer.is_stopped() and not $DashDelay.is_stopped():
+		$Placeholder.set_modulate(Color("Blue"))
+	if $DashDelay.is_stopped() and $KoyoteTimer.time_left > 0 and $KoyoteTimer.time_left != $KoyoteTimer.wait_time:
 		$Placeholder.set_modulate(Color("Yellow"))
-	if $DashTimer.is_stopped() and $DashDelay.is_stopped() and not is_on_floor() and DashToken <= 0 and $KoyoteTimer.is_stopped():
-		$Placeholder.set_modulate(Color("Red"))
-	if $DashTimer.is_stopped() and $DashDelay.is_stopped() and is_on_floor() or $DashTimer.is_stopped() and $DashDelay.is_stopped() and DashToken > 0 and $KoyoteTimer.is_stopped():
-		$Placeholder.set_modulate(Color("White"))
-	
+	if $DashDelay.is_stopped() and $WallJumpTimer.time_left > 0 and $WallJumpTimer.time_left != $WallJumpTimer.wait_time:
+			$Placeholder.set_modulate(Color("Purple"))
+	if $DashTimer.is_stopped() and $DashDelay.is_stopped() and not is_on_floor() and dash_token <= 0 and $KoyoteTimer.is_stopped():
+			$Placeholder.set_modulate(Color("Red"))
+	if $DashTimer.is_stopped() and $DashDelay.is_stopped() and is_on_floor()\
+	or $DashTimer.is_stopped() and $DashDelay.is_stopped() and dash_token > 0 and $KoyoteTimer.is_stopped() and $WallJumpTimer.is_stopped():
+			$Placeholder.set_modulate(Color("White"))
 	#Skew player face
 	if direction.x:
 		$Placeholder/Face.set_skew(deg_to_rad(13.5))
@@ -103,12 +170,12 @@ func Indicator(): # For testing and debugging
 	else:
 		$Placeholder/Face.set_skew(deg_to_rad(0))
 
-func Debug(): # Useful info
+func debug(): # Useful or interesting info.
 	if abs(velocity.x) > abs(topvelocity.x):
 		topvelocity.x = roundf(abs(velocity.x))
 	if abs(velocity.y) > abs(topvelocity.y):
 		topvelocity.y = roundf(abs(velocity.y))
 	$Debug.text = \
+	"Current State: "+str(_state) + "\n" + \
 	"Top Velocity.x: "+str(topvelocity.x) + "\n" + \
-	"Top Velocity.y: "+str(topvelocity.y) + "\n" + \
-	"Wall Normal: " + str(get_wall_normal().x)
+	"Top Velocity.y: "+str(topvelocity.y)
